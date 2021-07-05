e33a6365468b18f74e8695ade6e0d1e244206140
Jessica Temporal <jessica.temporal@auth0.com>
2021-07-05 11:17:42 -0300

    Reformats errors in decorators for improved readability

--- utils/decorators.py
+++ utils/decorators.py
@@ -35,43 +35,35 @@ def get_token():
 
     # Raise an error if no Authorization error is found
     if not authorization_header:
-        raise AuthError(
-            {
-                "code": "authorization_header_missing",
-                "description": "Authorization header is expected",
-            },
-            401,
-        )
+        payload = {
+            "code": "authorization_header_missing",
+            "description": "Authorization header is expected",
+        }
+        raise AuthError(payload, 401)
 
     authorization_header_parts = authorization_header.split()
 
     # We are expecting the Authorization header to contain a Bearer token
     if authorization_header_parts[0].lower() != "bearer":
-        raise AuthError(
-            {
-                "code": "invalid_header",
-                "description": "Authorization header must be a Bearer token",
-            },
-            401,
-        )
+        payload = {
+            "code": "invalid_header",
+            "description": "Authorization header must be a Bearer token",
+        }
+        raise AuthError(payload, 401)
 
     # The Authorization header is prefixed with Bearer,
     # but does not contain the actual token
     elif len(authorization_header_parts) == 1:
-        raise AuthError(
-            {"code": "invalid_header", "description": "Token not found"}, 401
-        )
+        payload = {"code": "invalid_header", "description": "Token not found"}
+        raise AuthError(payload, 401)
 
     # We only expect 2 parts, "Bearer" and the access token
     elif len(authorization_header_parts) > 2:
-        raise AuthError(
-            {
-                "code": "invalid_header",
-                "description":
-                "Authorization header must be a valid Bearer token",
-            },
-            401,
-        )
+        payload = {
+            "code": "invalid_header",
+            "description": "Authorization header must be a valid Bearer token",
+        }
+        raise AuthError(payload, 401)
 
     # If all checks out, we return the access token
     return authorization_header_parts[1]
@@ -89,10 +81,11 @@ def validate_token(token):
 
     # Check if the token has a key ID
     if "kid" not in unverified_header:
-        raise AuthError(
-            {"code": "missing_kid", "description": "No kid found in token"},
-            401
-        )
+        payload = {
+            "code": "missing_kid",
+            "description": "No kid found in token"
+        }
+        raise AuthError(payload, 401)
 
     try:
         # Check if we have a key with the key ID specified
@@ -122,45 +115,37 @@ def validate_token(token):
         # The token should be issued by our Auth0 tenant,
         # and to be used with our API (Audience)
         except jwt.JWTClaimsError:
-            raise AuthError(
-                {
-                    "code": "invalid_claims",
-                    "description":
+            payload = {
+                "code": "invalid_claims",
+                "description":
                     "Incorrect claims, please check the audience and issuer",
-                },
-                401,
-            )
+            }
+            raise AuthError(payload, 401)
 
         # The token's signature is invalid
         except jwt.JWTError:
-            raise AuthError(
-                {
-                    "code": "invalid_signature",
-                    "description": "The signature is not valid",
-                },
-                401,
-            )
+            payload = {
+                "code": "invalid_signature",
+                "description": "The signature is not valid",
+            }
+            raise AuthError(payload, 401)
 
         # Something went wrong parsing the JWT
         except Exception:
-            raise AuthError(
-                {
-                    "code": "invalid_header",
-                    "description": "Unable to parse authentication token.",
-                },
-                401,
-            )
+            payload = {
+                "code": "invalid_header",
+                "description": "Unable to parse authentication token.",
+            }
+            raise AuthError(payload, 401)
 
     except StopIteration:
         # We did not find the key with the ID specified in the token's header
         # in the list of available public keys for our Auth0 tenant.
-        raise AuthError(
-            {
-                "code": "invalid_header",
-                "description": "No valid public key found to validate signature.",
-            },
-            401,
-        )
+        payload = {
+            "code": "invalid_header",
+            "description": "No valid public key found to validate signature.",
+        }
+        raise AuthError(payload, 401)
 
 
 def requires_auth(f):
